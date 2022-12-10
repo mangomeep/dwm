02ea2351f7d47d92f9117fa1d7c864ec2ff0b2d8
mangomeep <nickdasdf@gmail.com>
2022-12-10 15:34:24 +1300

    colour changes

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#777700";
+static const char col_cyan[]        = "#066677";
 static const char col_urgborder[]   = "#ff0000";
 
 //#include "/home/ratking/.cache/wal/colors-wal-dwm.h"
